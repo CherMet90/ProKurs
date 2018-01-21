f64a5e8219755bae05fb69913dd58f4591171765
CherMet <dddfrdd@gmail.com>
2018-01-21 22:32:16 +0400

    tab adaptive

--- js/common.js
+++ js/common.js
@@ -1,5 +1,21 @@
 // alert ('jQuery подключен и отлично работает!');
 $(function() {
+
+	if ($(window).width()<369) {
+		$(".discounts__tab-titles--item").css('font-size', '4vw');
+	};
+
+	$(window).resize(function(){
+		switch (true) {
+			case ($(window).width()<369):
+				$(".discounts__tab-titles--item").css('font-size', '4vw');
+				break;
+			case ($(window).width()>=369):
+				$(".discounts__tab-titles--item").css('font-size', '16px');
+				break;
+		}
+	});
+
 	$(".top-line__address .tab").click(function() {
 		$(".top-line__address .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".address__phone .tab-item").hide().eq($(this).index()).fadeIn();
