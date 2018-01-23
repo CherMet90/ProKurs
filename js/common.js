315672405e47383150e2724c2ccbc382f745ad43
CherMet <dddfrdd@gmail.com>
2018-01-23 14:07:25 +0400

    headers second tab with auto-height

--- js/common.js
+++ js/common.js
@@ -1,6 +1,8 @@
 // alert ('jQuery подключен и отлично работает!');
 $(function() {
 
+	// heightAlignment(".discounts__content--wrap");
+
 	if ($(window).width()<369) {
 		$(".discounts__tab-titles--item").css('font-size', '4vw');
 	};
@@ -13,11 +15,33 @@ $(function() {
 			case ($(window).width()>=369):
 				$(".discounts__tab-titles--item").css('font-size', '16px');
 				break;
-		}
+		};
+		heightAlignment(".discounts__content--wrap");
 	});
 
 	$(".top-line__address .tab").click(function() {
 		$(".top-line__address .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".address__phone .tab-item").hide().eq($(this).index()).fadeIn();
 	}).eq(0).addClass("active");
+
+	$(".discounts__tab-titles--item").click(function() {
+		$(".discounts__content--wrap").css("display", "flex");
+		heightAlignment(".discounts__content--wrap");
+		$(".discounts__tab-titles--item").removeClass("active").eq($(this).index()).addClass("active");
+		$(".discounts__content--wrap").hide().eq($(this).index()).fadeIn();
+	}).eq(0).addClass("active");
+
+	function heightAlignment (elemsName) {
+		$(elemsName).css('height', '');
+		var max = $(elemsName).eq(0).height();
+		$(elemsName).each(function() {
+			if ($(this).height() > max) {
+				max = $(this).height();
+			};
+		});
+		$(elemsName).each(function() {
+			$(this).css('height', max);
+		});
+	};
+
 });
